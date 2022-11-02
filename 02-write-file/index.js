9f03b96679600b45b3d9509f22b64025beddc6e3
tat.bandelikova <tat.bandelikova@gmail.com>
2022-11-02 22:38:16 +0300

    feat: add solution with ctrl+c to 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout, stderr } = process;
+const output = fs.createWriteStream(path.join(__dirname, 'output.txt'), 'utf-8');
+
+stdout.write('Hello! You can leave your message here\n');
+
+stdin.on('data', (data) => {
+    if(data === 'exit') {
+        process.exit();
+    }
+    output.write(data);
+});
+
+process.on('exit', (code) => {
+    if(code === 0) {
+        stdout.write('Your message has been saved. Have a nice day!\n');
+    } else {
+        stderr.write(`Error code ${code}`);
+    }
+});
+process.on('SIGINT', () => process.exit());
+
+stdin.on('error', error => console.log('Error', error.message));
