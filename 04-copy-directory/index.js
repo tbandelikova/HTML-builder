8fb2d34e0bbe399cb5143525268f8f6dc487d45d
tat.bandelikova <tat.bandelikova@gmail.com>
2022-11-07 14:27:08 +0300

    feat: add solution to 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const { copyFile, mkdir, readdir } = require('fs/promises');
 
+(async function copyDir(sourcePath, destinationPath) {
+    try {
+        await mkdir(destinationPath, { recursive: true });
+        const files = await readdir(sourcePath, {withFileTypes: true});
+        for(const file of files) {
+            try {
+                if(file.isDirectory()) {
+                    await mkdir(path.join(__dirname, 'files-copy', file.name), { recursive: true });
+                } else {
+                    await copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), fs.constants.COPYFILE_EXCL);
+                }
+            } catch(err) {
+                if(err.code == 'EEXIST') {
+                    fs.rm(path.join(__dirname, 'files-copy', file.name), () => {
+                        copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name));
+                    });
+                } else {
+                    console.error(err);
+                }
+            }
+        }
+
+    } catch (err) {
+        console.error(err);
+    }
+})(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));
