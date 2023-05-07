59075513c0027a7eb46fc760596aa0a0a969de20
tat.bandelikova <tat.bandelikova@gmail.com>
2023-05-08 01:15:50 +0300

    fix: fix error in html-output 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,59 +2,37 @@ const fs = require('fs');
 const path = require('path');
 const { copyFile, mkdir, readdir } = require('fs/promises');
 
-(async function() {
+(async function () {
     try {
         await mkdir(path.join(__dirname, 'project-dist'), { recursive: true });
         const outputIndex = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'), 'utf-8');
         const outputStyle = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'), 'utf-8');
-        // fs.readFile(path.join(__dirname, 'template.html'), 'utf8', (err, data) => {
-        //     if(err) throw err;
-        //     let temp = data;
-        //     fs.readdir(path.join(__dirname, 'components'), {withFileTypes: true}, (err,data) => {
-        //         if(err) throw (err);
-                
-        //         for(const file of data) {
-                    
-        //             if(file.isFile()&&temp.includes(file.name.slice(0, file.name.indexOf('.')))) {
-        //                 let temp1 = temp;
-        //                 fs.readFile(path.join(__dirname, 'components', file.name), 'utf-8', (err, data) => {
-        //                     if(err) throw (err);
-        //                     temp1 = temp1.replace(`{{${file.name.slice(0, file.name.indexOf('.'))}}}`, data);
-        //                     console.log(temp1);
-        //                 })
-                        
-        //                 // outputIndex.write(temp);
-        //             }
-                    
-        //         }
-                
-        //     });
-
-        // });
         const input = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
         let temp = '';
+
         input.on('data', chunk => temp += chunk);
         input.on('end', () => {
-            fs.readdir(path.join(__dirname, 'components'), {withFileTypes: true}, (err,data) => {
-                if(err) throw (err);
-                for(const file of data) {
-                    if(file.isFile()&&temp.includes(file.name.slice(0, file.name.indexOf('.')))) {
+            fs.readdir(path.join(__dirname, 'components'), { withFileTypes: true }, (err, data) => {
+                if (err) throw (err);
+                for (const file of data) {
+                    let place = `{{${file.name.slice(0, file.name.indexOf('.'))}}}`;
+                    if (file.isFile() && temp.includes(place)) {
                         let component = '';
                         let stream = fs.createReadStream(path.join(__dirname, 'components', file.name), 'utf-8');
                         stream.on('data', chunk => component += chunk);
                         stream.on('end', () => {
-                            temp = temp.replace(`{{${file.name.slice(0, file.name.indexOf('.'))}}}`, component);
-                            outputIndex.write(temp);
+                            temp = temp.replace(place, component);
+                            if (!temp.includes('{{')) outputIndex.write(temp);;
                         });
                     }
                 }
             });
         })
 
-        fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}, (err,data) => {
-            if(err) throw (err);
-            for(const file of data) {
-                if(file.isFile()&&path.extname(file.name) == '.css') {
+        fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, data) => {
+            if (err) throw (err);
+            for (const file of data) {
+                if (file.isFile() && path.extname(file.name) == '.css') {
                     let stream = fs.createReadStream(path.join(__dirname, 'styles', file.name), 'utf-8');
                     stream.pipe(outputStyle);
                 }
@@ -63,7 +41,7 @@ const { copyFile, mkdir, readdir } = require('fs/promises');
 
         copyDir(path.join(__dirname, 'assets'), path.join(__dirname, 'project-dist', 'assets'));
 
-    } catch(err) {
+    } catch (err) {
         console.error(err);
     }
 })();
@@ -71,18 +49,18 @@ const { copyFile, mkdir, readdir } = require('fs/promises');
 async function copyDir(sourcePath, destinationPath) {
     try {
         await mkdir(destinationPath, { recursive: true });
-        const files = await readdir(sourcePath, {withFileTypes: true});
-        for(const file of files) {
+        const files = await readdir(sourcePath, { withFileTypes: true });
+        for (const file of files) {
             try {
-                if(file.isDirectory()) {
+                if (file.isDirectory()) {
                     let sourcePath = path.join(__dirname, 'assets', file.name);
                     let destinationPath = path.join(__dirname, 'project-dist', 'assets', file.name);
                     copyDir(sourcePath, destinationPath);
                 } else {
                     await copyFile(path.join(sourcePath, file.name), path.join(destinationPath, file.name), fs.constants.COPYFILE_EXCL);
                 }
-            } catch(err) {
-                if(err.code == 'EEXIST') {
+            } catch (err) {
+                if (err.code == 'EEXIST') {
                     fs.rm(path.join(destinationPath, file.name), () => {
                         copyFile(path.join(sourcePath, file.name), path.join(destinationPath, file.name));
                     });
